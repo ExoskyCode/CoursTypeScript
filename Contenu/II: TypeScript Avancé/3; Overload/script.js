0359b073b54577bd596815a868d072f64f987590
ExoskyCode <102408092+ExoskyCode@users.noreply.github.com>
2022-12-10 22:27:15 +0100

    II: TypeScript Avancé/ 2. Opérateurs

---
+++ Contenu/II: TypeScript Avancé/3; Overload/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// TODO ________ L'OPÉRATEUR "!" ________ TODO //
+const container = document.querySelector('.container');
+const user1 = {
+    title: "Def FrontEnd",
+    description: "Développeur de sites web",
+    salary: 30000
+};
+const user2 = {
+    title: "Def FrontEnd",
+    description: "Développeur de sites web",
+    salary: 30000
+};
+// ? Le "?" signifie que l'élément est optionnel, s'il n'est aps renseigné, ça ne bugera pas ( marche avec les arguments, les interfaces, les objets)
